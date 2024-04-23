dd21d93442286fbf924586423f69c9a6ac0c82f9
Sarah Rashed <10rasheds@gmail.com>
2024-04-23 13:27:12 -0400

    confused with indexing vector from reading::read_link_connections

--- final_project/src/main.rs
+++ final_project/src/main.rs
@@ -1,5 +1,10 @@
 use urlencoding::decode;
 mod reading; 
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 fn main() {
     let encoded = "%C3%81ed%C3%A1n_mac_Gabr%C3%A1in";
@@ -7,6 +12,12 @@ fn main() {
 
     println!("{}",decoded);
 
+    // TODO: change to relative path
     let read_in = reading::read_link_connections("C:/Programming for Data Science/DS210-Final-Project/wikispeedia_paths-and-graph/links.tsv");
-    println!("{:?}", read_in);
+    //println!("{:?}", read_in);
+
+    
+    let my_variable = vec![(String::from("hello"), String::from("world"))];
+    println!("Type of read_in: {}", type_of(&read_in));
+
 }
